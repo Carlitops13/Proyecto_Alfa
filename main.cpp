4de72962c67cec959c321d45e7babd0554d8c5b3
Carlos <carlos.puente2626@gmail.com>
2025-06-28 23:07:01 -0500

    commit prueba

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <QApplication>
 #include <QPushButton>
+#include <iomanip>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
+    button.move(50, 50);
+
+
     button.show();
     return QApplication::exec();
 }
